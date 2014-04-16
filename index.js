d4f6550a912112496ceb7856355cc850938db381
Daniel Fagnan <dnfagnan@gmail.com>
2014-04-16 03:27:01 -0600

    working
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -8,8 +8,92 @@ exports = module.exports = function(redis) {
   return app;
 };
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.errorHandler());
+app.use('/public', express.static(__dirname + '/public'));
+app.use(express.compress());
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.methodOverride());
 app.use(app.router);
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.render('index');
+});
+
+app.get('/groups', function(req, res) {
+  rollout.client.smembers(rollout.name('rollout:groups'), function(err, groups) {
+    if (err) {
+      return res.send(500);
+    }
+
+    groups.splice(0, 1);
+
+    res.render('groups', {
+      groups: groups
+    });
+  });
+});
+
+app.get('/groups/:id', function(req, res) {
+  var name = req.params.id;
+
+  rollout.client.smembers(rollout.name('rollout:groups:' + name), function(err, features) {
+    if (err) {
+      return res.send(500);
+    }
+
+    res.render('groups/show', {
+      features: features,
+      group: name
+    });
+  });
+});
+
+app.get('/groups/:id/features/add', function(req, res) {
+  var name = req.params.id;
+
+  res.render('groups/new', {
+    group: name
+  });
+});
+
+app.get('/groups/:id/features/:feature/remove', function(req, res) {
+  var group = req.params.id;
+  var feature = req.params.feature;
+
+  rollout.client.srem(rollout.name('rollout:groups:' + group), feature, function(err) {
+    res.redirect('/groups/' + group);
+  });
+});
+
+app.post('/groups/:id/features', function(req, res) {
+  var group = req.params.id;
+  var name  = req.body.feature;
+
+  if (!name) {
+    return res.send(400, {
+      status: 'error',
+      message: 'Missing feature name.'
+    });
+  }
+  rollout.group(group, function() { return true; });
+  rollout.group(group).activate(name).then(function() {
+    res.redirect('/groups/' + group);
+  });
+
+});
+
+
+app.get('/deactivate', function(req, res) {
+  res.render('deactivate');
+});
+
+app.post('/deactivate', function(req, res) {
+  var feature = req.body.feature;
+
+  rollout.deactivate(feature).then(function() {
+    res.redirect('/');
+  });
 });
